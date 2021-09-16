0e2b91b52eb2f45434af68052885ecf839bdd401
Frédéric Collonval <fcollonval@gmail.com>
2021-09-16 16:01:40 +0200

    Skip failing test on Windows Python 3.9

--- jupyterlab_pullrequests/tests/test_handlers_integration.py
+++ jupyterlab_pullrequests/tests/test_handlers_integration.py
@@ -1,3 +1,4 @@
+import sys
 from unittest.mock import patch
 
 import pytest
@@ -7,6 +8,7 @@ valid_prid = "https://api.github.com/repos/timnlupo/juypterlabpr-test/pulls/1"
 valid_prfilename = "test.ipynb"
 
 
+@pytest.mark.skipif(sys.platform == "win32" and (3, 9) <= sys.version_info < (3, 10), reason="Event loop error on Windows Python 3.9")
 @patch("jupyterlab_pullrequests.base.PRConfig.access_token", "")
 async def test_ListPullRequests_pat_empty(jp_fetch):
     with pytest.raises(
